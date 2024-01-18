f6370ad205bfc81d1da08e438366e8332ed71b5e
Rishabh007788 <148675909+Rishabh007788@users.noreply.github.com>
2024-01-18 17:59:13 +0530

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -5,33 +5,36 @@ var completelist = document.getElementById("completelist");
 
 function Addtask() {
 
-    
-    var task = input.value;
-    var li = document.createElement("li");
-    li.textContent = task;
-    var span = document.createElement("span");
-    span.innerHTML = "\u00d7"
+    if (input.value === '') {
+        alert("Task can't be empty")
+    } else {
+        var task = input.value;
+        var li = document.createElement("li");
+        li.textContent = task;
+        var span = document.createElement("span");
+        span.innerHTML = "\u00d7"
 
-    li.appendChild(span)
+        li.appendChild(span)
 
-    activelist.appendChild(li);
-    input.value=''
+        activelist.appendChild(li);
+        input.value = ''
+    }
 
 }
 
 activelist.addEventListener(
     "click", (e) => {
         var removed;
-        if(e.target.tagName == "LI") {
+        if (e.target.tagName == "LI") {
             e.target.classList.toggle("checked")
 
         }
-        else if(e.target.tagName == "SPAN") {
+        else if (e.target.tagName == "SPAN") {
             e.target.parentElement.remove();
-            
+
         }
-        
-        
+
+
     }
 )
 
