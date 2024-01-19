58bbc702874bb48d722b9bc61ba9b0e6151c25db
Rishabh007788 <148675909+Rishabh007788@users.noreply.github.com>
2024-01-19 18:15:38 +0530

    Update ToDo.js

--- ToDo.js
+++ ToDo.js
@@ -1,6 +1,8 @@
 var input = document.getElementById("input");
 var activelist = document.getElementById("activelist");
 var completelist = document.getElementById("completelist");
+var addbt = document.getElementById("addbt")
+
 
 
 function Addtask() {
@@ -8,34 +10,61 @@ function Addtask() {
     if (input.value === '') {
         alert("Task can't be empty")
     } else {
+        //store task in a varable..
         var task = input.value;
+        
         var li = document.createElement("li");
         li.textContent = task;
         var span = document.createElement("span");
+        span.className="removebtn"
         span.innerHTML = "\u00d7"
 
+        //create one btn for mark the task as complete..
+        var completebtn = document.createElement("span");
+        completebtn.className = "completebt";
+
+
         li.appendChild(span)
+        li.appendChild(completebtn)
 
         activelist.appendChild(li);
         input.value = ''
+        savetask()
     }
 
 }
 
+input.addEventListener(
+    "keypress",
+    (event)=>{
+        if (event.keyCode===13) {
+            Addtask()
+        }
+    }
+)
+
 activelist.addEventListener(
     "click", (e) => {
         var removed;
-        if (e.target.tagName == "LI") {
-            e.target.classList.toggle("checked")
-
+        if (e.target.className === "completebt") {
+            e.target.parentElement.classList.toggle("checked");
+            savetask()
         }
-        else if (e.target.tagName == "SPAN") {
+        else if (e.target.className === "removebtn") {
             e.target.parentElement.remove();
-
+            savetask();
         }
-
-
     }
 )
+
+function savetask() {
+    localStorage.setItem("tasks", activelist.innerHTML)
+}
+
+function displaytasks() {
+    activelist.innerHTML=localStorage.getItem("tasks");
+}
+
+displaytasks();
 
 
